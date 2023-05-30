e7cb11462c5c91a6528b4b9a1c4a650b1fc66745
Selfrules <mattia@selfrules.org>
2023-05-30 23:00:49 +0200

    GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown';
 
 /* 
   We are doing some URL mumbo jumbo here to tell Astro what the URL of your website will be.
@@ -33,7 +34,14 @@ export default defineConfig({
     port: SERVER_PORT
   },
   site: BASE_URL,
-  integrations: [sitemap(), tailwind({
+  integrations: [
+    sitemap(), 
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+    tailwind({
     config: {
       applyBaseStyles: false
     }
